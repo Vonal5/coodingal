5c99b365bd97d01ab702a1cbb959083ed2ab2abe
Vonal5 <sandunastron83@gmail.com>
2025-06-07 21:57:58 +0530

    at

---
+++ coodingal/web div coodingal/lectures/L7/intro.py
@@ -0,0 +1,9 @@
+lst = ["apple", "Guava", "Mango", "banana", "kiivi"]
+
+print("length of list:", lst(lst))
+print("first element:", lst[0])
+print("last element:", lst[-1])
+
+lst.append("papaya")
+
+
